47bd2d45ad763f8ab88bc948c71087eb6803c5cf
Hugo Courthias <hugo.courthias@gmail.com>
2023-02-22 17:22:49 +0100

    add eslint / prettier

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,27 @@
-module.exports = function(api) {
-  api.cache(true);
+/* eslint-disable no-undef */
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias: {
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@navigation': './src/navigation',
+            '@locales': './src/locales',
+            '@helpers': './src/helpers',
+            '@hooks': './src/hooks',
+            '@constants': './src/constants',
+            '@theme': './src/theme',
+            '@store': './src/store',
+          },
+        },
+      ],
+    ],
+  }
+}
