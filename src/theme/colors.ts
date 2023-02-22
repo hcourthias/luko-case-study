96a6867b76f3b971368be33cbca783bc72a328c4
Hugo Courthias <hugo.courthias@gmail.com>
2023-02-22 17:22:15 +0100

    add missing color

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -1,5 +1,12 @@
 export const colors = {
-    mainBlue: '#2D50E6',
-    mainGrey: '#C0BEB8',
-    background: '#F4F3EF',
+  mainBlue: '#2D50E6',
+  gray: {
+    100: '#EAE9E3',
+    300: '#C0BEB8',
+    700: '#6B6651',
+    1000: '#2C2302',
+  },
+  background: '#F4F3EF',
+  white: '#FFFFFF',
+  black: '#000000',
 }
